b6749e424d08b21e7651253219575afb41e3c3a4
ilun <1445709137@qq.com>
2023-05-24 16:34:40 +0800

    clone

--- modules/clone.js
+++ modules/clone.js
@@ -3,7 +3,11 @@ import isArray from './isArray.js';
 import extend from './extend.js';
 
 // Create a (shallow-cloned) duplicate of an object.
+// 浅拷贝对象
 export default function clone(obj) {
-  if (!isObject(obj)) return obj;
-  return isArray(obj) ? obj.slice() : extend({}, obj);
+  // 不是对象，直接返回
+  if (!isObject(obj)) return obj
+  // 是数组调用数组的slice方法进行浅拷贝，否则使用自定义extend方法创建浅拷贝
+  return isArray(obj) ? obj.slice() : extend({}, obj)
 }
+
